10790f9d25463aba4bb54cfac18f447ba61335d7
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-11-21 23:54:35 +0900

    fix cors

--- scripts/server.py
+++ scripts/server.py
@@ -22,6 +22,7 @@ class TopicResource(Resource):
         date, keyword
         keyword=kw&dates=2011-03-11&dates=2011-03-12&
         '''
+        print('request in')
         bigwords = ['福島', '福島県', '原発', '福島原発', '東電', '放射能', '放射線']
         params = parameters.split('&')
         keyword = params[0].split('=')[1]
@@ -36,7 +37,7 @@ class TopicResource(Resource):
                 d['words'] for d in corpuslist if keyword in d['words']]
             corlist += [d for d in corpuslist if keyword in d['words']]
             kwlist += [d[0] for d in kwscorelist]
-        _, roots = extract_sentences(wordslist, kwlist, thres=50)
+        _, roots = extract_sentences(wordslist, kwlist, thres=30)
         graph = generate_graph(roots, kwlist)
         return [graph, corlist]
 
@@ -45,4 +46,4 @@ api.add_resource(TopicResource, '/topic/<parameters>')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='127.0.0.1', port=port)
